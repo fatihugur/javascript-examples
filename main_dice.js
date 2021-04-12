c437e7257dc98e08b40ab4dc9ad6fb6760453e30
Fatih Ugur <fatih.ugur@appwise.be>
2021-04-12 15:45:34 +0200

    second dubblesteen

--- main_dice.js
+++ main_dice.js
@@ -3,13 +3,19 @@
 //console.log(Math.random(0,6));
 
 var random1 = Math.random();
+var random2  = Math.random();
 if(random1 < 1){
 	//console.log("dice is smaller than 1");
 	random1 = (random1+1) * 6; // no zero in random
+	random2 = (random2+1) * 6;
 	random1 = Math.floor(random1); // integer
+	random2 = Math.floor(random2); 
 	if(random1 > 6) {
 		random1 = random1 % 6; // integer between 1, 2, 3, 4, 5, 6
-
+	}
+	if(random2 > 6) {
+		random2 = random2 % 6;  // integer for random2
 	}
 }
 console.log(random1);
+console.log(random2);
