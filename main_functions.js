7b0ea054f2bb093aa0152d08b131ee332899aefc
Fatih Ugur <fatih.ugur@appwise.be>
2021-04-13 14:33:50 +0200

    array function relations

--- main_functions.js
+++ main_functions.js
@@ -22,3 +22,31 @@ console.log("Function in Function");
  var doIetsResult = doIets(8,10);
  var doIetsResult2 = doIets(doIets(9,11),10);
  console.log(doIetsResult);
+
+
+//ADD NAME
+
+ console.log("Function add");
+
+ function addName(name, surname){
+ 	var addedName = name + " " + surname;
+ 	return addedName;
+ }
+
+  console.log(addName("Fatih", "Ugur"));
+
+  // ARRAY ADD NAME SURNAME
+
+  var names = ["Fatih ", "Yavuz ", "Ali ", "Emir "]; // text array
+  var surnames = ["Ugur", "Demir", "Yildiz", "Oz"]; // text array
+  for(var counter=0; counter <names.length; counter++){ //for loop
+  	var random = Math.floor(Math.random()*4);  // random
+  	var random1 =  Math.floor(Math.random()*4); // random1
+  	console.log(addName(names[random],surnames[random1])); //addName function
+  }
+
+
+
+
+
+
